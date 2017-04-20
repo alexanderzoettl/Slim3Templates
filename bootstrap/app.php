e959ce95ef32b59d0c3d2bc027c0a7528d7115e3
alexanderzoettl <alexander.zoettl@gmail.com>
2017-04-20 16:17:34 +0200

    change2.0

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,4 +58,8 @@ $container['HomeController'] = function($container){
 	return new \App\Controllers\HomeController($container);
 };
 
+$container['AboutController'] = function($container){
+	return new \App\Controllers\HomeController($container);
+};
+
 require __DIR__ . '/../app/routes.php';
