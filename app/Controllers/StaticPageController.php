03f63686d1cbe399b424fef2b215498aa050e59f
Daniel Englisch <xeroaustria@hotmail.com>
2017-04-25 17:01:57 +0200

    removed mailer classes (will be replaced)

--- app/Controllers/StaticPageController.php
+++ app/Controllers/StaticPageController.php
@@ -9,6 +9,16 @@ class StaticPageController extends Controller
 
 	public function home($request, $response)
 	{		
+
+		$res = $this->mailer->send('templates/mail.twig', ['test' => 'HALLOTEST', 'test2' => 'HALLOTEST2'] , function($message){
+      $message->to('xeroaustria@hotmail.com');
+      $message->subject('Email Subject');
+      $message->from('mailer@xeroserver.org'); // if you want different sender email in mailer call function
+      $message->fromName('Sender Name'); // if you want different sender name in mailer call function
+	});
+		var_dump($res);
+		die;
+
 		return $this->view->render($response, 'home.twig');
 	}
 	
